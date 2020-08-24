a604cfb9eac3f0487852937332578efe4cf208ac
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-08-24 22:29:41 +0100

    movie css module fix

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 interface Prop {
 	rating: string;
@@ -8,18 +9,21 @@ const Filters: React.FC<Prop> = ({ rating, onChange }) => {
 	const ratings = ['00', '10', '20', '30', '40', '50', '60', '70', '80', '90'];
 
 	return (
-		<div className='ratings'>
-			<p>Minimum Rating : </p>
-			{ratings.map((r, i) => (
-				<span
-					key={i}
-					onClick={() => onChange(r.charAt(0))}
-					className={parseInt(r) < parseInt(rating) * 10 ? 'disabled' : ''}
-				>
-					{r}
-				</span>
-			))}
-		</div>
+		<>
+			<Button>Click Here</Button>
+		</>
+		// <div className='ratings'>
+		// 	<p>Minimum Rating : </p>
+		// 	{ratings.map((r, i) => (
+		// 		<span
+		// 			key={i}
+		// 			onClick={() => onChange(r.charAt(0))}
+		// 			className={parseInt(r) < parseInt(rating) * 10 ? 'disabled' : ''}
+		// 		>
+		// 			{r}
+		// 		</span>
+		// 	))}
+		// </div>
 	);
 };
 
