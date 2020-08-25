21f628a442ea286e687ca99568eeac762fe54258
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-08-25 18:53:05 +0100

    genres

--- components/GenresList.tsx
+++ components/GenresList.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import { Button } from 'semantic-ui-react';
 
 const fetchGenres = async (__key, endpoint) => {
 	const res = await fetch(endpoint);
 	return res.json();
 };
-
 export const GenresList = ({ endpoint, onChange, active }) => {
 	const { data, status } = useQuery(['genres', endpoint], fetchGenres);
 	return (
@@ -13,13 +13,14 @@ export const GenresList = ({ endpoint, onChange, active }) => {
 			{status === 'success' &&
 				data.genres.map(genre => {
 					return (
-						<span
+						<Button
+							className='genre'
 							key={genre.id}
 							onClick={() => onChange(genre)}
-							className={active == genre.id ? 'active' : ''}
+							active={genre.id == active}
 						>
 							{genre.name}
-						</span>
+						</Button>
 					);
 				})}
 		</div>
