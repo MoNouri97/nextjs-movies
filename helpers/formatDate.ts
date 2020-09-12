6a6eed35049413ebfddcb5781f6b4a9e2851a630
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-09-12 14:38:35 +0100

    error handling for formatDate

--- helpers/formatDate.ts
+++ helpers/formatDate.ts
@@ -1,19 +1,21 @@
-export default function formatReleaseDate(releaseDate: string): string {
-	const date = new Date(releaseDate);
-	const dateTimeFormat = new Intl.DateTimeFormat('en', {
-		year: 'numeric',
-		month: 'long',
-		day: '2-digit',
-	});
-	const [
-		{ value: month },
-		,
-		{ value: day },
-		,
-		{ value: year },
-	] = dateTimeFormat.formatToParts(date);
-
-	// console.log(`${day}-${month}-${year}`);
-
-	return `${month} ${year}`;
+export default function formatDate(releaseDate: string): string {
+	try {
+		const date = new Date(releaseDate);
+		const dateTimeFormat = new Intl.DateTimeFormat('en', {
+			year: 'numeric',
+			month: 'long',
+			day: '2-digit',
+		});
+		const [
+			{ value: month },
+			,
+			{ value: day },
+			,
+			{ value: year },
+		] = dateTimeFormat.formatToParts(date);
+		// console.log(`${day}-${month}-${year}`);
+		return `${month} ${year}`;
+	} catch (error) {
+		return '';
+	}
 }
